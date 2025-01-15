1a138ae0f081b45f4ad8408876082164dd258fdc
kimEHwoo <156396847+kimEHwoo@users.noreply.github.com>
2025-01-15 21:04:01 +0900

    Title: 좀 더 어려운 수학 점수, Time: 42ms, Memory: 16MB, Status: Passed - CodeTree

--- 250115/좀 더 어려운 수학 점수/math-scores-are-more-difficult.py
+++ 250115/좀 더 어려운 수학 점수/math-scores-are-more-difficult.py
@@ -5,12 +5,7 @@ arr_B = inp_B.split()
 math_A, eng_A = int(arr_A[0]), int(arr_A[1])
 math_B, eng_B = int(arr_B[0]), int(arr_B[1])
 
-if math_A > math_B:
+if math_A > math_B or (math_A==math_B and eng_A>eng_B):
     print("A")
-elif math_A < math_B:
-    print("B")
-else:
-    if eng_A > eng_B:
-        print("A")
-    elif eng_A < eng_B:
-        print("B")
+else:   
+     print("B")
