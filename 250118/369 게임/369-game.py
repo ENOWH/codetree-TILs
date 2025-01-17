3aefbd068957de8493eb4e8b957ec8d9d84355c8
kimEHwoo <156396847+kimEHwoo@users.noreply.github.com>
2025-01-18 01:04:43 +0900

    Title: 369 게임, Time: 42ms, Memory: 16MB, Status: Passed - CodeTree

--- 250118/369 게임/369-game.py
+++ 250118/369 게임/369-game.py
@@ -1,12 +1,11 @@
 n = int(input())
 
 for i in range(1, n+1):
-    s = str(i)
-    count = 0
-    for x in s:
-        if x=='3' or x=='6' or x=='9':
-            count += 1
-    if count>0 or i%3==0:
-        print(0, end = " ")
+    if i%3==0:
+        print("0", end=" ")
+    elif i%10==3 or i%10==6 or i%10==9:
+        print("0", end=" ")
+    elif i//10==3 or i//10==6 or i//10==9:
+        print("0", end=" ")
     else:
-        print(i, end = " ")
+        print(i, end=" ")
