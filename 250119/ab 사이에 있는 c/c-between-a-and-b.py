7f4dec9c6adabc86b43e12231acb5e9506da46c9
kimEHwoo <156396847+kimEHwoo@users.noreply.github.com>
2025-01-19 17:38:04 +0900

    Title: ab 사이에 있는 c, Time: 95ms, Memory: 19MB, Status: Runtime Error - CodeTree

--- 250119/ab 사이에 있는 c/c-between-a-and-b.py
+++ 250119/ab 사이에 있는 c/c-between-a-and-b.py
@@ -2,7 +2,7 @@ a, b, c = input().split()
 a, b, c = int(a), int(b), int(c)
 
 for i in range(a, b+1):
-    if i%5==0:
+    if i%c==0:
         satisfied = True
 if satisfied==True:
     print("YES")
