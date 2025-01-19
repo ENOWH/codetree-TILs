d79f72804aae4e72a23a69e49d10778fcfeab550
kimEHwoo <156396847+kimEHwoo@users.noreply.github.com>
2025-01-20 00:23:49 +0900

    Title: 모두 3의 배수, Time: 44ms, Memory: 16MB, Status: Wrong Answer - CodeTree

---
+++ 250120/모두 3의 배수/all-multiples-of-3.py
@@ -0,0 +1,15 @@
+satisfied=True
+cnt=1
+
+while True:
+    cnt+=1
+    n = int(input())
+    if n%3!=0:
+        satisfied=False
+    if cnt>=5:
+        break
+
+if satisfied==True:
+    print("1")
+else:
+    print("0")
