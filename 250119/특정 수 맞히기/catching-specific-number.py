bd12c77ace89b8e487de23b16ba102f6e7c0bb89
kimEHwoo <156396847+kimEHwoo@users.noreply.github.com>
2025-01-19 15:08:20 +0900

    Title: 특정 수 맞히기, Time: 44ms, Memory: 16MB, Status: Passed - CodeTree

--- 250119/특정 수 맞히기/catching-specific-number.py
+++ 250119/특정 수 맞히기/catching-specific-number.py
@@ -1,9 +1,9 @@
 while True:
-    n = int(input())
-    if n<=25:
-        print("Higher")
+    n=int(input())
+    if n==25:
+        print("Good")
+        break
     elif n>=25:
         print("Lower")
     else:
-        print("Good")
-        break
+        print("Higher")
