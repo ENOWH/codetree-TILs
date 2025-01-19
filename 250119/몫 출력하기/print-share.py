bdabec2e69b462326d586e73a41441388cbce9c1
kimEHwoo <156396847+kimEHwoo@users.noreply.github.com>
2025-01-19 16:20:01 +0900

    Title: 몫 출력하기, Time: 79ms, Memory: 18MB, Status: Runtime Error - CodeTree

--- 250119/몫 출력하기/print-share.py
+++ 250119/몫 출력하기/print-share.py
@@ -1,9 +1,9 @@
-cnt=0
+cnt = 0
 while True:
-    n=int(input())
+    n = int(input())
     if n%2==1:
-        cnt+=1
-    else:
+        pass
+    elif n%2==0:
         cnt+=1
         print(n//2)
     if cnt>3:
