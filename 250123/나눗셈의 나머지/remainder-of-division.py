51009ebedbf1f7b5d444633580f4cf84ecf18b6d
kimEHwoo <156396847+kimEHwoo@users.noreply.github.com>
2025-01-23 02:11:37 +0900

    Title: 나눗셈의 나머지, Time: 44ms, Memory: 16MB, Status: Passed - CodeTree

--- 250123/나눗셈의 나머지/remainder-of-division.py
+++ 250123/나눗셈의 나머지/remainder-of-division.py
@@ -2,14 +2,12 @@ arr = [0]*10
 a, b = map(int, input().split())
 
 while a>1:
-    a//=b
-    remainder = a%b
     for i in range(10):
-        if remainder==i:
+        if i==a%b:
             arr[i]+=1
+    a//=b
 
 sum_val=0
-
-for i in range (10):
-    sum_val+=(arr[i]*arr[i])
+for elem in arr:
+    sum_val+=(elem**2)
 print(sum_val)
