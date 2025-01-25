a1d629f1c28780af64cf3744955c4e7b851d9810
kimEHwoo <156396847+kimEHwoo@users.noreply.github.com>
2025-01-25 15:22:02 +0900

    Title: 배열 놀이, Time: 50ms, Memory: 16MB, Status: Passed - CodeTree

--- 250125/배열 놀이/play-with-array.py
+++ 250125/배열 놀이/play-with-array.py
@@ -3,14 +3,17 @@ nums = list(map(int, input().split()))
 
 for _ in range(q):
     arr = list(map(int, input().split()))
-    num = arr[0]
-    a = arr[1]
-    if num == 1:
-        print(nums[a-1])
-    elif num == 2:
-        print(nums.index(a)+1)
-    elif num == 3:
-        b = arr[2]
-        for i in range(a, b+1):
+
+    if arr[0] == 1:
+        print(nums[arr[1]-1])
+
+    elif arr[0] == 2:
+        if arr[1] not in nums:
+            print(0)
+        else:
+            print(nums.index(arr[1])+1)
+
+    elif arr[0] == 3:
+        for i in range(arr[1], arr[2]+1):
             print(nums[i-1], end = " ")
         print()
