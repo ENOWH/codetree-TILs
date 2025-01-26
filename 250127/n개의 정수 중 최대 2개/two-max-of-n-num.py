a5ee19e4cc823ddf74174c474b447351342c3e0b
kimEHwoo <156396847+kimEHwoo@users.noreply.github.com>
2025-01-27 06:42:53 +0900

    Title: n개의 정수 중 최대 2개, Time: 50ms, Memory: 16MB, Status: Passed - CodeTree

--- 250127/n개의 정수 중 최대 2개/two-max-of-n-num.py
+++ 250127/n개의 정수 중 최대 2개/two-max-of-n-num.py
@@ -3,9 +3,11 @@ array = list(map(int, input().split()))
 
 n = len(array)
 
-for i in range(1, n):
-    for j in range(i, 0, -1):
-        if array[j-1] < array[j]:
-            array[j-1], array[j] = array[j], array[j-1]
+for i in range(n-1):
+    min_index=i
+    for j in range(min_index+1, n):
+        if array[min_index] < array[j]:
+            min_index = j
+    array[i], array[min_index] = array[min_index], array[i]
 
 print(array[0], array[1])
