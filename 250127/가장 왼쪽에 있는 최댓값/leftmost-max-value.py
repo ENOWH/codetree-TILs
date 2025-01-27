55f3d047ef7be01f044ea421739119b3a7991be2
kimEHwoo <156396847+kimEHwoo@users.noreply.github.com>
2025-01-27 21:56:19 +0900

    Title: 가장 왼쪽에 있는 최댓값, Time: 58ms, Memory: 17MB, Status: Passed - CodeTree

--- 250127/가장 왼쪽에 있는 최댓값/leftmost-max-value.py
+++ 250127/가장 왼쪽에 있는 최댓값/leftmost-max-value.py
@@ -1,18 +1,14 @@
 n = int(input())
 arr = list(map(int, input().split()))
 
-prev_max_idx = n
+prev_max_index = n
 
 while True:
     max_idx = 0
-
-    for i in range(1, prev_max_idx):
+    for i in range(1, prev_max_index):
         if arr[i] > arr[max_idx]:
             max_idx = i
-
-    print(max_idx + 1, end = " ")
-
-    if max_idx==0:
+    print(max_idx+1, end = " ")
+    if max_idx == 0:
         break
-
-    prev_max_idx = max_idx
+    prev_max_index = max_idx
