5ab55ebc4ba540615af03eb2d7359fee09750e04
ENOWH <156396847+ENOWH@users.noreply.github.com>
2025-02-17 16:51:40 +0900

    Title: 재귀함수를 이용한 최댓값, Time: 48ms, Memory: 16MB, Status: Passed - CodeTree

--- 250217/재귀함수를 이용한 최댓값/maximum-value-with-recursive-function.py
+++ 250217/재귀함수를 이용한 최댓값/maximum-value-with-recursive-function.py
@@ -1,14 +1,9 @@
+N = int(input())
+array = list(map(int, input().split()))
+
 def find_max(arr):
-    # 기저 조건: 리스트에 원소가 하나만 남으면 그 값이 최댓값
-    if len(arr) == 1:
+    if len(arr)==1:
         return arr[0]
-    
-    # 첫 번째 원소와 나머지 원소 중 최댓값 비교
     return max(arr[0], find_max(arr[1:]))
 
-# 입력 처리
-n = int(input())  # 원소 개수
-arr = list(map(int, input().split()))  # 리스트 입력
-
-# 최댓값 출력
-print(find_max(arr))
+print(find_max(array))
