e12d5e6d0dce28a1f7aa98af50edecc254919c64
ENOWH <156396847+ENOWH@users.noreply.github.com>
2025-02-21 16:50:05 +0900

    Title: 왔다 갔던 구역 2, Time: 46ms, Memory: 16MB, Status: Passed - CodeTree

--- 250221/왔다 갔던 구역 2/area-been-to-and-from2.py
+++ 250221/왔다 갔던 구역 2/area-been-to-and-from2.py
@@ -1,23 +1,25 @@
-n = int(input())
-x = []
-dir = []
-for _ in range(n):
-    xi, di = input().split()
-    x.append(int(xi))
-    dir.append(di)
+N = int(input())
 
-ans = [0 for _ in range(1001)]
+arr = [0 for _ in range (1001)]
+offset = 500
 
-for i in range(n):
-    if dir[i] == "L":
-        for j in range(x[i]):
-            ans[j+500] += 1
-    else:
-        for j in range(x[i]):
-            ans[j+500] += 1
-    
-cnt = 0
-for i in range(len(ans)):
-    if ans[i]>=2:
-        cnt += 1
-print(cnt)
+for i in range(N):
+    go, cmd = map(str, input().split())
+    go = int(go)
+
+    if cmd == 'R':
+        for j in range(go):
+            arr[offset + j] += 1
+        offset += go
+
+    if cmd == 'L':
+        for j in range(1, go+1):
+            arr[offset - j] += 1
+        offset -= go
+
+count = 0
+for i in range(len(arr)):
+    if arr[i] >= 2:
+        count += 1
+        
+print(count)
