8321eb8640905b96f0e607df2105ed0ab7d27b0f
ENOWH <156396847+ENOWH@users.noreply.github.com>
2025-02-21 22:59:53 +0900

    Title: 흰검 칠하기, Time: 43ms, Memory: 16MB, Status: Wrong Answer - CodeTree

--- 250221/흰검 칠하기/painting-white-black.py
+++ 250221/흰검 칠하기/painting-white-black.py
@@ -1,35 +1,29 @@
-OFFSET = 100000
-MAX_R = 200000
+n = int(input())  # 명령 개수
+commands = [input().split() for _ in range(n)]  # 명령 리스트
 
-n = int(input())
-arr_color = [0 for _ in range(MAX_R+1)]
-arr_cnt = [0 for _ in range(MAX_R+1)]
+position = 0  # 현재 위치
+tiles = {}  # {위치: 칠해진 횟수}
 
-for _ in range(n):
-    distance, direction = tuple(input().split())
-    distance = int(distance)
-
+for command in commands:
+    distance, direction = int(command[0]), command[1]
+    
     if direction == 'L':
-        for j in range(0, -distance, -1):
-            arr_color[OFFSET+j] = 'L'
-            arr_cnt[OFFSET+j] +=1
-        OFFSET -= (distance-1)
+        for i in range(position, position - distance, -1):  # 왼쪽으로 이동
+            tiles[i] = tiles.get(i, 0) + 1
+        position -= (distance - 1)  # 마지막 위치 반영
 
     elif direction == 'R':
-        for j in range(distance):
-            arr_color[OFFSET+j] = 'R'
-            arr_cnt[OFFSET+j] +=1
-        OFFSET += (distance-1)
+        for i in range(position, position + distance):  # 오른쪽으로 이동
+            tiles[i] = tiles.get(i, 0) + 1
+        position += (distance - 1)  # 마지막 위치 반영
 
-white, black, gray = 0,0,0
+# 색상 개수 카운트
+white, black, gray = 0, 0, 0
 
-for i in range(MAX_R+1):
-    if arr_cnt[i]>=4:
+for count in tiles.values():
+    if count >= 2:
         gray += 1
-    elif arr_cnt[i] >= 1:
-        if arr_color[i] == 'L':
-            white += 1
-        elif arr_color[i] == 'R':
-            black += 1
+    elif count == 1:
+        black += 1  # 마지막에 칠해진 색이 검은색('R')이므로 기본값이 검은색
 
 print(white, black, gray)
