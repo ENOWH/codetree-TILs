f2d12a91d492d0d6d749b753040391078fa77c38
ENOWH <156396847+ENOWH@users.noreply.github.com>
2025-02-21 13:51:44 +0900

    Title: 여러가지 진수변환, Time: 42ms, Memory: 16MB, Status: Passed - CodeTree

---
+++ 250221/여러가지 진수변환/various-numeral-system-transformations.py
@@ -0,0 +1,12 @@
+N, B = tuple(map(int, input().split()))
+digits = []
+
+while True:
+    if N<B:
+        digits.append(N)
+        break
+    digits.append(N%B)
+    N //= B
+
+for digit in digits[::-1]:
+    print(digit, end = "")
