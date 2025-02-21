c703cc943c65bd691dc3d1eeca9f9e24126c51d3
ENOWH <156396847+ENOWH@users.noreply.github.com>
2025-02-21 15:05:55 +0900

    Title: 최대로 겹치는 구간, Time: 51ms, Memory: 16MB, Status: Wrong Answer - CodeTree

---
+++ 250221/최대로 겹치는 구간/maximum-overlapped-segments.py
@@ -0,0 +1,10 @@
+n = int(input())
+ans = [0] * 300
+
+for _ in range(n):
+    x1, x2 = tuple(map(int, input().split()))
+
+    for i in range(x1+100, x2+100+1):
+        ans[i] += 1
+    
+print(max(ans))
